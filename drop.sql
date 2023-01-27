804a6e1c842193ac4de85c0d59aeb51fa0b160db
= <rat00@iesemilidarder.com>
2023-01-28 00:06:07 +0100

    delete and drop

---
+++ drop.sql
@@ -0,0 +1,11 @@
+drop table incidents;
+drop table rent;
+drop table users;
+drop table bike_stock;
+drop table additional_features_stock;
+drop table additional_features_size;
+drop table sizes;
+drop table bikes;
+drop table additional_features;
+/*MariaDB [database_bikes]> show tables;
+Empty set (0.001 sec)*/
